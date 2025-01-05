d7d87648b84654490372e93f4d9cac70c0407822
Madeeha Khan <madeehakhan16@gmail.com>
2025-01-05 13:46:47 -0500

    comments and syntax edits

--- CSharp/PalindromeNumber_Easy.cs
+++ CSharp/PalindromeNumber_Easy.cs
@@ -17,17 +17,23 @@ public class Solution
     {
         if (x < 0)
             return false;
-        int pow10 = GetMagnitude(x) - 1;
+        int pow10 = GetMagnitude(x) - 1; //how big is our number
         while (x > 0)
         {
             double mag = Math.Pow(10, pow10);
-            int leftDig = (int)(x / mag);
-            int rightDig = (int)(x % 10);
-            if (!(leftDig == rightDig))
+            int leftDig = (int)(x / mag); //find the first digit of x
+            int rightDig = (int)(x % 10); //find the las digit of x
+            if (leftDig != rightDig)
                 return false;
-            pow10 = pow10 - 2;
-            x = (int)((x - leftDig * mag) / 10);
+            pow10 = pow10 - 2; //looked at the largest and smallest digits
+            x = (int)((x - leftDig * mag) / 10); //remove the largest and smallest digits
         }
         return true;
     }
 }
+
+//I didn't bother with the string method
+//GetMagnitude is borrowed
+//Getting the left digit was really easy conceptually but there isn't a built-in method to get the scientific notation of a number or its magnitude in C#
+//Getting the right digit tripped me up because i tried to % it by a different number but I just needed 10
+//Fun!!!
--- CSharp/TwoSum_Easy.cs
+++ CSharp/TwoSum_Easy.cs
@@ -5,17 +5,19 @@ public class Solution {
         Dictionary<int, int> dict = new Dictionary<int, int>();
         int[] res = new int[2];
         for (int i = 0; i < nums.Length; i++) {
-            int diff = target - nums[i];
-            Console.WriteLine("{0}, {1}", i, diff);
-            if (dict.ContainsKey(nums[i])) {
-                Console.WriteLine(dict[nums[i]]);
+            int diff = target - nums[i]; //find the complement of this number
+            if (dict.ContainsKey(nums[i])) { //if our number is already in our record, we just found its complement
                 res = [i, dict[nums[i]]];
                 return res;
             }
             else {
-                if (!dict.ContainsKey(diff)) dict.Add(diff, i);
+                if (!dict.ContainsKey(diff)) dict.Add(diff, i); //otherwise we need to keep track of where we found the complement of any possible pair
             }
         }
         return res;
     }
+
+    //Easy once you understand how dictionaries work -- recording information that could be useful for later and instant lookup
+    //The only optimization I see in this method is only computing diff if we are in the else case
+    //I got stuck looking up dict[diff] instead of dict[num] but drawing out the problem helped 
 
